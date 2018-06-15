caa04f8bcf6a32ea79392e15622dff00a8122923
Unknown <kevin.phommavanh@group-hpi.com>
2018-06-15 15:26:34 +0200

    Graph

--- Singe.py
+++ Singe.py
@@ -21,6 +21,30 @@ class Banana :
         self.color = color
 
 
+class Graph :
+    def __init__(self,nodes,bridges):
+        self.nodes = nodes
+        self.bridges = bridges
+
+
+    def addNode(self,node):
+        self.nodes.append(node)
+
+    def addBridge(self,nodeA,nodeB):
+        self.bridges[nodeA].append(nodeB)
+        self.bridges[nodeB].append(nodeA)
+
+
+
+    def isNodesConnected(self,nodeA,nodeB):
+        if nodeA in self.bridges[nodeB] and nodeB in self.bridges[nodeA]:
+            return "true"
+        else:
+            return "false"
+
+
+
+
 pierre = Monkey("Pierre")
 bananeVerte = Banana("verte")
 marie = Monkey("Marie")
@@ -30,5 +54,25 @@ marie.eat(bananeJaune)
 pierre.eat(bananeVerte)
 
 robert = pierre.reproduceWith(marie, "Robert")
+
+
+
+keymap = {"A" : ["B","C"],
+          "B" : ["A"],
+          "C" : ["A"]}
+nodes = ["A", "B", "C"]
+
+graph = Graph(nodes,keymap)
+
+print( "A et B connecté ? : " + graph.isNodesConnected("A","B"))
+
+print("C et B connecté ? " + graph.isNodesConnected("C","B"))
+
+graph.addBridge("C","B")
+
+print("C et B connecté ? " + graph.isNodesConnected("C","B"))
+
+
+
 
 
