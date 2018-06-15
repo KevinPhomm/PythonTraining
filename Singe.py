47450d1f536146d327c1a639c9c699cb9e2ee467
Unknown <kevin.phommavanh@group-hpi.com>
2018-06-15 14:45:49 +0200

    exo monkey

---
+++ Singe.py
@@ -0,0 +1,34 @@
+import time
+
+
+class Monkey :
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, banana):
+        print(self.name + " mange une banane de couleur " + banana.color)
+
+    def reproduceWith(self, monkey, childrenName):
+        print("wow c'est chaud entre " + self.name + " et " + monkey.name + "!")
+        robert = Monkey(childrenName)
+        time.sleep(5)
+        print(robert.name + " est né !")
+        return robert
+
+
+class Banana :
+    def __init__(self, color):
+        self.color = color
+
+
+pierre = Monkey("Pierre")
+bananeVerte = Banana("verte")
+marie = Monkey("Marie")
+bananeJaune = Banana("Jaune")
+
+marie.eat(bananeJaune)
+pierre.eat(bananeVerte)
+
+robert = pierre.reproduceWith(marie, "Robert")
+
+
